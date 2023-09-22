207a9ee7e733c5cc13a3ba2945aecc271224a229
Mars-chao <k110705701.mg10@nycu.edu.tw>
2023-09-22 13:27:23 +0800

    [renew] menu修改

--- jQuery/jquery.js
+++ jQuery/jquery.js
@@ -1,20 +1,47 @@
 $(document).ready(function () {
-    $(".product").click(function (event) {
-      event.preventDefault();
-      $(this).toggleClass('active');
-      $(".dropdownOpenProduct li").slideToggle();
+  $(".product").click(function (event) {
+    event.preventDefault();
+
+    //上方li固定顏色
+    $(this).toggleClass('active');
+
+    // 開啟當前選單
+    $(this).next().toggleClass('active');
     //   $(this).parent().siblings().find("ul").slideUp();
-     });
-    $(".contract").click(function (event) {
-      event.preventDefault();
-      $(this).toggleClass('active');
-      // $(this).parent().siblings().find("ul").slideUp();
-      $(".dropdownOpenContent li").slideToggle();
-    });
-    $('.top a').click(function(event){
-      event.preventDefault();
-      $('html, body').animate({
-        scrollTop: 0
-      }, 700);
-    })
   });
+
+
+  $(".contract").click(function (event) {
+    event.preventDefault();
+    
+    $(this).toggleClass('active');
+    $(this).next().toggleClass('active');
+    // $(this).parent().siblings().find("ul").slideUp();
+  });
+
+  // 視窗滑動 => top 按鈕會顯現
+  $(window).scroll(function () {
+    if ($(window).scrollTop() >= 100) {
+      $(".top").fadeIn(250);
+    }
+    else {
+      $(".top").fadeOut(250);
+    }
+  });
+
+});
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  speed: 500,
+  loop: true,
+  autoplay: {
+    delay: 1500,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
